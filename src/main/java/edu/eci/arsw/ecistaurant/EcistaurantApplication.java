3d9afb30fb4da2a85914f72019c7aa9ec10249c6
Andres Felipe Marcelo <53924254+AndresMarcelo7@users.noreply.github.com>
2020-03-09 21:24:09 -0500

    Commit Inicial

---
+++ src/main/java/edu/eci/arsw/ecistaurant/EcistaurantApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ecistaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcistaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcistaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/ecistaurant/EcistaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.ecistaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcistaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

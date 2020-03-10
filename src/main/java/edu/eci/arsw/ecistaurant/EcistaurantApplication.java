484c99e6a38f64fed93c4a0fa30d7c95c945c5d8
JohanS11 <johan.arias@mail.escuelaing.edu.co>
2020-03-10 09:49:15 -0500

    ??

--- src/main/java/edu/eci/arsw/ecistaurant/EcistaurantApplication.java
+++ src/main/java/edu/eci/arsw/ecistaurant/EcistaurantApplication.java
@@ -1,22 +1,15 @@
 package edu.eci.arsw.ecistaurant;
-
-import edu.eci.arsw.ecistaurant.controllers.EcistaurantController;
-import edu.eci.arsw.ecistaurant.model.Estudiante;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories
 @ComponentScan
 public class EciStaurantApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(EciStaurantApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(EciStaurantApplication.class, args);
-
-
-	}
+    }
 
 }

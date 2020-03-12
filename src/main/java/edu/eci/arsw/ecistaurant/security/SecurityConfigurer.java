c79cd6db21fa7bf0b018579a3e030356550d11d0
JohanS11 <53972478+JohanS11@users.noreply.github.com>
2020-03-12 09:51:42 -0500

    Spring Security Hasta SecurityConfigurer

--- src/main/java/edu/eci/arsw/ecistaurant/security/SecurityConfigurer.java
+++ src/main/java/edu/eci/arsw/ecistaurant/security/SecurityConfigurer.java
@@ -3,25 +3,27 @@ package edu.eci.arsw.ecistaurant.security;
 import edu.eci.arsw.ecistaurant.services.MyUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@EnableWebSecurity
+
+@Configuration
 public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private MyUserDetailsService myUserDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myUserDetailsService);
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    @Override
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.myUserDetailsService).passwordEncoder(passwordEncoder());
     }
+
 }

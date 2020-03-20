1f5745fad2587fb4c6e1e4c1bbbf56e124b6088d
JohanS11 <johan.arias@mail.escuelaing.edu.co>
2020-03-19 22:46:11 -0500

    Adding swagger

--- src/main/java/edu/eci/arsw/ecistaurant/EciStaurantApplication.java
+++ src/main/java/edu/eci/arsw/ecistaurant/EciStaurantApplication.java
@@ -2,11 +2,12 @@ package edu.eci.arsw.ecistaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @ComponentScan
+@EnableSwagger2
 public class EciStaurantApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EciStaurantApplication.class, args);
 

d1bc235e1f2a67802a09d21c3bfd4d03c9b91d31
Andres Felipe Marcelo <53924254+AndresMarcelo7@users.noreply.github.com>
2020-03-21 17:17:13 -0500

    añadiendo logIn y SecurityConfs

---
+++ src/main/java/edu/eci/arsw/ecistaurant/controllers/HomeController.java
@@ -0,0 +1,35 @@
+package edu.eci.arsw.ecistaurant.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = { "/" }, method = RequestMethod.GET)
+    public String start(Model model) {
+        return "index";
+    }
+
+
+    @RequestMapping(value = { "/welcome" }, method = RequestMethod.GET)
+    public String welcome(Model model) {
+        return "welcome";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+
+
+}
+
--- src/main/java/edu/eci/arsw/ecistaurant/security/MyUserDetailsService.java
+++ src/main/java/edu/eci/arsw/ecistaurant/security/MyUserDetailsService.java
@@ -1,4 +1,4 @@
-package edu.eci.arsw.ecistaurant.services;
+package edu.eci.arsw.ecistaurant.security;
 
 import edu.eci.arsw.ecistaurant.model.Usuario;
 import edu.eci.arsw.ecistaurant.persistence.UsuarioRepository;
@@ -14,8 +14,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
